b0d6040b360e729d04b0eeb9f58cee630d7ff27e
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2023-05-25 20:02:32 +0700

    Membenarkan tulisan yang salah

--- Doublelinkedlist_103/Doublelinkedlist_103.cpp
+++ Doublelinkedlist_103/Doublelinkedlist_103.cpp
@@ -109,7 +109,7 @@ void DoubleLinkedList::traverse() {
 	if (listEmpty())
 		cout << "\nList is empty" << endl;
 	else {
-		cout << "\nRecord in traverse order of roll number are: " << endl;
+		cout << "\nRecord in ascending order of roll number are: " << endl;
 		Node* currentNode = START;
 		while (currentNode != NULL) {
 			cout << currentNode->noMhs << " " << currentNode->nama << endl;
@@ -122,7 +122,7 @@ void DoubleLinkedList::revtraverse() {
 	if (listEmpty())
 		cout << "\nList is empty" << endl;
 	else {
-		cout << "\nRecord in revtraverse order of roll number are: " << endl;
+		cout << "\nRecord in descending order of roll number are: " << endl;
 		Node* currentNode = START;
 		while (currentNode != NULL) {
 			cout << currentNode->noMhs << " " << currentNode->nama << endl;
@@ -175,8 +175,8 @@ int main() {
 			cout << "\nMenu" << endl;
 			cout << "1. Add a record to the list" << endl;
 			cout << "2. Delete a record from the list" << endl;
-			cout << "3. view all record in the traverse order of roll" << endl;
-			cout << "4. view all record in the revtraverse order of roll" << endl;
+			cout << "3. view all record in the ascending order of roll" << endl;
+			cout << "4. view all record in the descending order of roll" << endl;
 			cout << "5. Search for a record in the list" << endl;
 			cout << "6. Exit" << endl;
 			cout << "\nEnter your choice (1-6): ";
