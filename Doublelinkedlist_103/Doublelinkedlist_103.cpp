7b2bbb71f778757609ef77ce07bfbd9027ff0390
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2023-05-25 19:18:17 +0700

    Membuat pilihan perintah untuk dijalankan 1 - 6

--- Doublelinkedlist_103/Doublelinkedlist_103.cpp
+++ Doublelinkedlist_103/Doublelinkedlist_103.cpp
@@ -135,4 +135,22 @@ void DoubleLinkedList::revtraverse() {
 		}
 	}
 }
-	
+
+int main() {
+	DoubleLinkedList obj;
+	while (true) {
+		try {
+			cout << "\nMenu" << endl;
+			cout << "1. Add a record to the list" << endl;
+			cout << "2. Delete a record from the list" << endl;
+			cout << "3. view all record in the traverse order of roll" << endl;
+			cout << "4. view all record in the revtraverse order of roll" << endl;
+			cout << "5. Search for a record in the list" << endl;
+			cout << "6. Exit" << endl;
+			cout << "\nEnter your choice (1-6): ";
+			char ch;
+			cin >> ch;
+
+		}
+	}
+}
